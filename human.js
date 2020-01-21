b54684800e29704c5f3b55058ca195f2a755ed8e
John Ramos <N00172468@iadt.ie>
2020-01-21 11:04:12 +0000

    Render

--- human.js
+++ human.js
@@ -1,5 +1,11 @@
 class Human {
     constructor() {
-        
+
+    }
+
+    render() {
+        fill(0, 0, 255);
+        ellipse(250, 250, 50, 50);
     }
 }
+
--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-function setup() {
+let humans = [];
+let numOfHumans = 1;
 
+function setup() {
+    createCanvas(500, 500);
+    background(122);
+
+    for (let i = 0; i < numOfHumans; i++) {
+        humans.push(new Human(i));
+    }
 }
 
 function draw() {
-
+    background(122);
+    humans.forEach(human => {
+        human.render();
+    });
 }
