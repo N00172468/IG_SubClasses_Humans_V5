8497bbbe7578c72a03aa77dfeb3b642acfda2e3c
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2020-01-27 15:45:58 +0000

    Added Stroke

--- human.js
+++ human.js
@@ -4,8 +4,11 @@ class Human {
     }
 
     render() {
+        push(); // Start a new drawing state
+        stroke(120,161,255);
         fill(0, 0, 255);
         ellipse(250, 250, 50, 50);
+        pop(); // Restore original state
     }
 }
 
